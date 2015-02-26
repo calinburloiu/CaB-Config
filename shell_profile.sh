f76b7353b5a11c43a6185ceb380fb79b22e5eb32
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2015-02-26 23:36:49 +0200

    Correct mistakes.

--- shell_profile.sh
+++ shell_profile.sh
@@ -1,8 +1,8 @@
-alias clipboard=xclip -selection clipboard
+alias clipboard="xclip -selection clipboard"
 
 # Set local Hadoop environment.
-export HADOOP_HOME=${HOME}/opt/hadoop
-export HIVE_HOME=${HOME}/opt/hive
-export SPARK_HOME=${HOME}/opt/spark
+export HADOOP_HOME="${HOME}/opt/hadoop"
+export HIVE_HOME="${HOME}/opt/hive"
+export SPARK_HOME="${HOME}/opt/spark"
 # Set PATH.
 export PATH="${PATH}:${HOME}/opt/hadoop/bin::${HOME}/opt/hive/bin:${HOME}/opt/spark/bin"
